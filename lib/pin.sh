863f3b48c891cb84507cc259c444d7bd9fa7ac35
sgohl <8873017+sgohl@users.noreply.github.com>
2024-04-03 15:21:42 +0200

    Update pin.sh

--- lib/pin.sh
+++ lib/pin.sh
@@ -1,11 +1,11 @@
 PinsAll() {
 
-	if [[ $(ls -1 db/pin | wc -l) -eq 0 ]]
+	if [[ -d ${DBF}/pin ]] && [[ $(ls -1 ${DBF}/pin | wc -l) -eq 0 ]]
 	then
 		return 1
 	fi
 	
-	for PIN in $(ls -1 db/pin/*)
+	for PIN in $(ls -1 ${DBF}/pin/*)
 	do
 		echo ${PIN}
 	done
